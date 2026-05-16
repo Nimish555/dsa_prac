bed32349e62392be5cbfdfbb9325b896bc99bac7
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-05-16 22:29:11 +0530

    add

---
+++ src/consOnes.java
@@ -0,0 +1,33 @@
+public class consOnes {
+
+    static void main(String[] args) {
+
+        int[] arr = {1, 0, 1, 1, 0, 1, 1, 1};
+
+        oness(arr);
+    }
+
+    static void oness(int[] arr){
+
+        int max = 0;
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] == 1){
+
+                count++;
+
+                if(count > max){
+                    max = count;
+                }
+
+            } else {
+
+                count = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
