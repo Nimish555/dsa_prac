4f7a2b35e65a8a839ba5be144dd343af7915c807
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-09 23:20:15 +0530

    add

---
+++ src/countDigiAlhpa.java
@@ -0,0 +1,12 @@
+//public class countDigiAlhpa {
+//    static void main(String[] args) {
+//        String str = "dgIo54mo%^&*";
+//        int digi = 0;
+//        int alpha = 0 ;
+//        int sp =0;
+//        for(int i = 0 ; i<str.length() ; i++){
+//            char ch = str.charAt(i);
+//        }
+//        if((ch >= 'a'&& ch<= )
+//    }
+//}
