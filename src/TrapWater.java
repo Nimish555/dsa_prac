c017f0b69031f8f209864cbcae0ec74e216878af
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-07-04 18:41:17 +0530

    push

--- src/TrapWater.java
+++ src/TrapWater.java
@@ -1,21 +1,41 @@
-public class TrapWater {
-    static void main(String[] args) {
-        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
-        int totalWater = 0;
-        for(int i = 0 ; i<arr.length ; i++){
-            int leftmax = arr[i];
-            for(int j=0 ; j<=i ; j++){
-                leftmax = Math.max(leftmax , arr[j]);
-            }
-            int rightmax = arr[i];
-            for(int j = arr.length - 1 ; j>= i ; j--){
-                rightmax = Math.max(rightmax , arr[j]);
-            }
-            int water = Math.min(leftmax , rightmax) - arr[i];
-            if(water > 0){
-                totalWater += water;
-            }
-        }
-        System.out.println(totalWater);
-    }
-}
+////public class TrapWater {
+////    static void main(String[] args) {
+////        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+////        int totalWater = 0;
+////        for(int i = 0 ; i<arr.length ; i++){
+////            int leftmax = arr[i];
+////            for(int j=0 ; j<=i ; j++){
+////                leftmax = Math.max(leftmax , arr[j]);
+////            }
+////            int rightmax = arr[i];
+////            for(int j = arr.length - 1 ; j>= i ; j--){
+////                rightmax = Math.max(rightmax , arr[j]);
+////            }
+////            int water = Math.min(leftmax , rightmax) - arr[i];
+////            if(water > 0){
+////                totalWater += water;
+////            }
+////        }
+////        System.out.println(totalWater);
+////    }
+////}
+//
+//
+//public class TrapWater {
+//    static void main(String[] args) {
+//        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+//        int left =0;
+//        int right = arr.length - 1;
+//        int leftmax = 0;
+//        int rightmax = 0;
+//        int ans = 0;
+//        while(left < right){
+//            if()
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
